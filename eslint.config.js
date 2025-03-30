d0248783ae718b4d8a630f2f1473f71973dc444a
siljemogster <125352923+siljemogster@users.noreply.github.com>
2025-03-30 17:43:09 +0200

    Fix ESLint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,25 +1,16 @@
-module.exports = [
-    {
-      files: ["**/*.js"],
-      languageOptions: {
-        ecmaVersion: 2022,
-        sourceType: "module",
-        globals: {
-          // Test globals
-          describe: "readonly",
-          it: "readonly",
-          test: "readonly",
-          expect: "readonly",
-          beforeEach: "readonly",
-          afterEach: "readonly",
-          beforeAll: "readonly",
-          afterAll: "readonly",
-          jest: "readonly"
-        }
-      },
-      rules: {
-        "semi": ["error", "always"],
-        "quotes": ["error", "single"]
-      }
-    }
-  ];
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    semi: ['error', 'always'],
+    quotes: ['error', 'single'],
+  },
+};
