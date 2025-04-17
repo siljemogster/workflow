0ce4a3656f230250178c1fd0a91b33c2c7b23efa
siljemogster <125352923+siljemogster@users.noreply.github.com>
2025-04-17 09:09:18 +0100

    add test js

--- js/utils/add.test.js
+++ js/utils/add.test.js
@@ -1,5 +1,6 @@
 import { expect, test } from 'vitest';
-import { add } from './add.js';
+// eslint-disable-next-line no-unused-vars
+import { add } from './add';
 
 test('adds 1 + 2 to equal 3', () => {
   expect(1 + 2).toBe(3);
